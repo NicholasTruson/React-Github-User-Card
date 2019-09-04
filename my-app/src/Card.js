03e5ccb56eabde77f38c1f0fc05a7b323cb52e71
Nicholas Truson <nicholas.truson@gmail.com>
2019-09-04 10:07:00 -0700

    card component for app

---
+++ my-app/src/Card.js
@@ -0,0 +1,38 @@
+// *** Imports *** //
+
+import React from 'react'
+import 'semantic-ui-css/semantic.min.css'
+import { Segment, Card, Image } from 'semantic-ui-react'
+import CardContent from './CardContent'
+
+// *** Card Component *** //
+
+const Card = props => {
+    console.log(props)
+    return (<Segment raised>
+        <Card>
+            <Image src={props.data.avatar_url} />
+            <Card.Content>
+
+                <Card.Header>
+                    {props.data.login}
+                </Card.Header>
+
+                <Card.Meta>{props.data.html_url}</Card.Meta>
+                <Card.Meta>Location: {props.data.location}</Card.Meta>
+                <Card.Meta>Following: {props.data.following}</Card.Meta>
+
+                <Card.Description>Created at: {props.data.created_at}</Card.Description>
+
+                <h4>Followers:</h4>
+
+                <CardContent />
+
+            </Card.Content>
+            
+        </Card>
+
+    </Segment>)
+}
+
+export default Card;
